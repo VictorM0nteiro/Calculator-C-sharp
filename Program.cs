4f0472f891fc6e5ee5f0d9e20ed79def8198d124
Victor Monteiro <devvictor.io@gmail.com>
2024-08-08 14:44:49 -0300

    1st commit - initial code

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System; // Não esquecer de utilizar o ; no final, função System que possibilita utilizar o COnsole
+ 
+namespace Calculator 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Clear();// Limpar o terminal e possibilitar a o comando "Clear"
+
+            Console.WriteLine("Primeiro valor: ");
+            // string v1 = Console.ReadLine(); // Fazendo a leitura dos valores digitados 
+            // double pValor = Convert.ToDouble(v1); 
+            double pValor = float.Parse(Console.ReadLine()); // Outro método para fazer a mesma coisa;
+            Console.WriteLine(pValor);
+
+        }
+    }
+}
