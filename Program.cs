e5e17c43d72d283122e572f52352f4e0c418203e
Victor Monteiro <devvictor.io@gmail.com>
2024-08-12 15:27:28 -0300

    Second commit

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
 ﻿using System; // Não esquecer de utilizar o ; no final, função System que possibilita utilizar o COnsole
- 
+ // git reset no para remover ou desfazer algo
 namespace Calculator 
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Console.Clear();// Limpar o terminal e possibilitar a o comando "Clear"
+            Console.Clear();// Limpar o terminal e possibilitar a o comando "Clear"
 
-            Console.WriteLine("Primeiro valor: ");
             // string v1 = Console.ReadLine(); // Fazendo a leitura dos valores digitados 
             // double pValor = Convert.ToDouble(v1); 
-            double pValor = float.Parse(Console.ReadLine()); // Outro método para fazer a mesma coisa;
-            Console.WriteLine(pValor);
+            Console.WriteLine("Primeiro valor: ");
+            double priValor = double.Parse(Console.ReadLine()); // Outro método para fazer a mesma coisa;
+
+            Console.WriteLine("Segundo valor: ");
+            double secValor = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            double resultado = priValor + secValor;
+            Console.WriteLine("O resultado da soma é: " + resultado); // concatenação na impressão utilizar "+"
+            Console.WriteLine($"O resultado da soma é: {resultado}"); // outro método, interpolação de strings
+            Console.WriteLine($"O resultado da soma é: {priValor + secValor}");
+            Console.WriteLine("O resultado da soma é: " + (priValor + secValor));
 
         }
     }
--- obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f0472f891fc6e5ee5f0d9e20ed79def8198d124")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
