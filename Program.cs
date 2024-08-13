fec0f5ec25d7d09429978ecf5c2a5a0546a426f5
Victor Monteiro <devvictor.io@gmail.com>
2024-08-13 14:52:52 -0300

    4th commit
    pratically the same of the other commit but now I made anther function 'subtracao' subtraction

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Soma();
+            Subtracao();
 
         }
 
@@ -24,10 +24,35 @@ namespace Calculator
 
             Console.WriteLine("");
             double resultado = priValor + secValor;
+            
             //Console.WriteLine("O resultado da soma é: " + resultado); // concatenação na impressão utilizar "+"
             Console.WriteLine($"O resultado da soma é: {resultado}"); // outro método, interpolação de strings
             // Console.WriteLine($"O resultado da soma é: {priValor + secValor}");
             // Console.WriteLine("O resultado da soma é: " + (priValor + secValor));
+            Console.ReadKey();
         }
+
+        static void Subtracao(){
+            Console.Clear();
+            
+            Console.WriteLine("Primeiro valor:");
+            double priValor = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            double secValor = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            double resultado = priValor - secValor;
+
+            Console.WriteLine($"O resultado da subtração é: {resultado}");
+            Console.ReadKey(); // pausa a execução do programa até que o usuário pressione uma tecla, espera a interação para tomar a próxima ação.
+            // nessa caso serve só para o programa não encerrar automaticamente sozinho
+        }
+    
+    
+    
     }
+
+
 }
