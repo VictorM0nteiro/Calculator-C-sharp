be3fb670633d9140c71116e91f1da033d7f78f4d
Victor Monteiro <devvictor.io@gmail.com>
2024-08-16 15:47:00 -0300

    final commit
    I have finished the calculator, its basic although it works well

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System; // Não esquecer de utilizar o ; no final, função System que possibilita utilizar o COnsole
  // git reset no para remover ou desfazer algo
 namespace Calculator 
@@ -7,7 +6,53 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Multiplicacao();
+            Menu();
+        }
+        static void Menu(){
+            Console.Clear();
+
+            while(true){
+                Console.WriteLine("Qual operação deseja realizar?");
+                Console.WriteLine("1 - soma");
+                Console.WriteLine("2 - subtração");
+                Console.WriteLine("3 - Divisão");
+                Console.WriteLine("4 - Multiplicação");
+
+                Console.WriteLine("------------");
+                Console.WriteLine("Selecione uma opção: ");
+                short opSelecionada = short.Parse(Console.ReadLine());
+            
+                if(opSelecionada == 1 ){
+                    Soma();
+                }
+                else if(opSelecionada == 2){
+                    Subtracao();
+                }
+                else if(opSelecionada == 3){
+                    Divisao();
+                }
+                else if(opSelecionada == 4){
+                    Multiplicacao();
+                }
+                else{
+                    Console.WriteLine("Opção inexistente");
+                }
+                Console.WriteLine("Deseja reiniciar o processo?");
+                Console.WriteLine("1 - Sim");
+                Console.WriteLine("0 - Não");
+                short reinicio = short.Parse(Console.ReadLine());
+
+                if( reinicio == 0){
+                    Console.WriteLine("Encerrando Processo");
+                    break;
+                }
+                else if( reinicio != 1){
+                    Console.WriteLine("Opção inválida. Reiniciando...");
+                }
+            }
+
+
+            //Console.ReadKey();
         }
 
         static void Soma(){
@@ -16,6 +61,7 @@ namespace Calculator
 
             // string v1 = Console.ReadLine(); // Fazendo a leitura dos valores digitados 
             // double pValor = Convert.ToDouble(v1); 
+            Console.WriteLine("Selecionado Soma: ");
             Console.WriteLine("Primeiro valor: ");
             double priValor = double.Parse(Console.ReadLine()); // Outro método para fazer a mesma coisa;
 
@@ -35,6 +81,7 @@ namespace Calculator
         static void Subtracao(){
             Console.Clear();
             
+            Console.WriteLine("Selecionado Subtração: ");
             Console.WriteLine("Primeiro valor:");
             double priValor = double.Parse(Console.ReadLine());
 
@@ -53,6 +100,7 @@ namespace Calculator
         static void Divisao(){
             Console.Clear();
 
+            Console.WriteLine("Selecionado Divisão: ");
             Console.WriteLine("Primeiro valor:");
             double priValor = double.Parse(Console.ReadLine());
 
@@ -70,6 +118,7 @@ namespace Calculator
         static void Multiplicacao(){
             Console.Clear();
 
+            Console.WriteLine("Selecionado Multiplicação: ");
             Console.WriteLine("Primeiro valor:");
             double priValor = double.Parse(Console.ReadLine());
 
@@ -88,95 +137,4 @@ namespace Calculator
     }
 
 
-=======
-﻿using System; // Não esquecer de utilizar o ; no final, função System que possibilita utilizar o COnsole
- // git reset no para remover ou desfazer algo
-namespace Calculator 
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Multiplicacao();
-        }
-
-        static void Soma(){
-
-            Console.Clear();// Limpar o terminal e possibilitar a o comando "Clear"
-
-            // string v1 = Console.ReadLine(); // Fazendo a leitura dos valores digitados 
-            // double pValor = Convert.ToDouble(v1); 
-            Console.WriteLine("Primeiro valor: ");
-            double priValor = double.Parse(Console.ReadLine()); // Outro método para fazer a mesma coisa;
-
-            Console.WriteLine("Segundo valor: ");
-            double secValor = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("");
-            double resultado = priValor + secValor;
-            
-            //Console.WriteLine("O resultado da soma é: " + resultado); // concatenação na impressão utilizar "+"
-            Console.WriteLine($"O resultado da soma é: {resultado}"); // outro método, interpolação de strings
-            // Console.WriteLine($"O resultado da soma é: {priValor + secValor}");
-            // Console.WriteLine("O resultado da soma é: " + (priValor + secValor));
-            Console.ReadKey();
-        }
-
-        static void Subtracao(){
-            Console.Clear();
-            
-            Console.WriteLine("Primeiro valor:");
-            double priValor = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Segundo valor:");
-            double secValor = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("");
-
-            double resultado = priValor - secValor;
-
-            Console.WriteLine($"O resultado da subtraçao é: {resultado}");
-            Console.ReadKey(); // pausa a execução do programa até que o usuário pressione uma tecla, espera a interação para tomar a próxima ação.
-            // nessa caso serve só para o programa não encerrar automaticamente sozinho
-        }
-
-        static void Divisao(){
-            Console.Clear();
-
-            Console.WriteLine("Primeiro valor:");
-            double priValor = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Segundo valor:");
-            double secValor = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("");
-
-            double resultado = priValor / secValor;
-
-            Console.Write($"O resultado da divisao é: {resultado}");
-            Console.ReadKey();
-        }
-
-        static void Multiplicacao(){
-            Console.Clear();
-
-            Console.WriteLine("Primeiro valor:");
-            double priValor = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Segundo valor:");
-            double secValor = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("");
-
-            double resultado = priValor * secValor;
-
-            Console.WriteLine($"O resultado da multiplicaçao é: {resultado}");
-            Console.ReadKey();
-        }
-    
-    
-    }
-
-
->>>>>>> ad8f051880ab35fe559e0f7c0ce83966bab48b78
 }
--- obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fec0f5ec25d7d09429978ecf5c2a5a0546a426f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+596a63c474eea659c91d97e64b1c775ce9c894f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
